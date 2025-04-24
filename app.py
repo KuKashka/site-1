e242c8b1aa1f85373b7ce74252f3ee1f9d639816
KuKashka <ostovrah@gmail.com>
2025-04-24 19:36:17 +0300

    gg

--- app.py
+++ app.py
@@ -10,6 +10,13 @@ def index():
     return render_template("index.html", articles=articles)  # html-сторінка, що повертається у браузер
 
 
+@app.route("/article/<int:article_id>")  # Вказуємо url-адресу для виклику функції
+def article_page(article_id):
+    article = get_article(article_id)
+    return render_template("article_page.html", article=article)  # html-сторінка, що повертається у браузер
+
+
+
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True  # автоматичне оновлення шаблонів
     app.run(debug=True)  # Запускаємо веб-сервер з цього файлу в режимі налагодження
--- sql_sripts.py
+++ sql_sripts.py
@@ -9,3 +9,13 @@ def get_all_aeticles():
     conn.close()
 
     return articles
+
+def get_article(article_id):
+    conn = sqlite3.connect('blog.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM articles WHERE id=?', [article_id])
+    articles = cursor.fetchone()
+    conn.close()
+
+    return articles
